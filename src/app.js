da7f3d3ad321f940d1644dc87258e91713a4a9ec
Vion-sis <79034712+Vion-sis@users.noreply.github.com>
2022-12-09 17:12:52 +0900

    updated the units conversion

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 function showCurrentTemperature(response) {
   console.log(response);
-  let temperature = Math.round(response.data.main.temp);
   let humidity = response.data.main.humidity;
   let wind = Math.round(response.data.wind.speed);
   let countryName = response.data.sys.country;
   let description = response.data.weather[0].description;
   let name = response.data.name;
 
+  celciusTemperature = Math.round(response.data.main.temp);
+
   let currentHumidity = document.querySelector("#current-humidity");
   currentHumidity.innerHTML = `Humidity: ${humidity}%`;
   let currentTemperature = document.querySelector("#current-temp");
-  currentTemperature.innerHTML = `${temperature}`;
+  currentTemperature.innerHTML = `${celciusTemperature}`;
   let currentWindSpeed = document.querySelector("#current-wind");
   currentWindSpeed.innerHTML = `Wind: ${wind}m/s`;
   let cityCountry = document.querySelector("#country");
@@ -19,6 +20,11 @@ function showCurrentTemperature(response) {
   currentDescription.innerHTML = `${description}`;
   let cityDisplayName = document.querySelector("#name");
   cityDisplayName.innerHTML = `${name}`;
+  let iconDisplay = document.querySelector("#icon");
+  iconDisplay.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function search(cityName) {
@@ -35,8 +41,33 @@ function handleSubmit(event) {
   search(cityName);
 }
 
+function showFahTemperature(event) {
+  event.preventDefault();
+
+  let fahTemperature = Math.round((celciusTemperature * 9) / 5 + 32);
+  let currentTemperature = document.querySelector("#current-temp");
+  currentTemperature.innerHTML = `${fahTemperature}`;
+  celciusUnit.classList.remove("active");
+  fahUnit.classList.add("active");
+}
+
+function showCelciusTemperature(event) {
+  event.preventDefault();
+
+  let currentTemperature = document.querySelector("#current-temp");
+  currentTemperature.innerHTML = celciusTemperature;
+}
+
+let celciusTemperature = null;
+
 let cityValue = document.querySelector("#search-text");
 cityValue.addEventListener("submit", handleSubmit);
+
+let fahUnit = document.querySelector("#fah");
+fahUnit.addEventListener("click", showFahTemperature);
+
+let celciusUnit = document.querySelector("#celcius");
+celciusUnit.addEventListener("click", showCelciusTemperature);
 
 let now = new Date();
 
